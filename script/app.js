b79a9bfa011862151c28817dbcf70e14bed59807
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-08-28 12:59:12 +0300

    checkbox toggleer

--- script/app.js
+++ script/app.js
@@ -1,2 +1,19 @@
+'use strict';
+
+// checkbox
+
+const checkbox = document.querySelectorAll('#discount-checkbox');
+
+checkbox.forEach( function(item) {
+  item.addEventListener( 'click', function() {
+    if (this.checked) {
+      this.nextElementSibling.classList.add('checked');
+    } else {
+      this.nextElementSibling.classList.remove('checked');
+    }
+    
+  })
+})
+
 
 
