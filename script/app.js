5cc237bf5befb7f486a97250f8159ed58f3c2839
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-08-28 14:46:23 +0300

    add to cart function created

--- script/app.js
+++ script/app.js
@@ -15,5 +15,44 @@ checkbox.forEach( function(item) {
   })
 })
 
+// cart
+
+const cartButton = document.querySelector('#cart');
+const cartWindow = document.querySelector('.cart');
+const cartClose  = document.querySelector('.cart-close');
+
+cartButton.addEventListener( 'click', function() {
+  cartWindow.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+})
+
+cartClose.addEventListener( 'click', () => {
+  cartWindow.style.display = 'none';
+  document.body.style.overflow = '';
+})
+
+
+// add to cart
+
+const goodsCard = document.querySelectorAll('.goods .card');
+const cartInner = document.querySelector('.cart-wrapper');
+const cartStatus = document.querySelector('#cart-empty');
+
+goodsCard.forEach( (card) => {
+  let addToCartButton = card.querySelector('.btn')
+  addToCartButton.addEventListener( 'click', () => {
+    let clonedCard = card.cloneNode(true);
+    cartInner.appendChild(clonedCard)
+    cartStatus.remove();
+    cartLength();
+  })
+})
+
+function cartLength() {
+  let cartCounter = document.querySelector('.counter');
+  let amountCardsInCart = document.querySelectorAll('.cart-wrapper .card')
+  cartCounter.innerHTML = amountCardsInCart.length
+}
+
 
 
